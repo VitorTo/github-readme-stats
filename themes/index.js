297f48c69349bf0ec9f20045c7eee1ad415ecd36
Vitor Gabriel <72284310+VitorTo@users.noreply.github.com>
2021-11-11 14:50:59 -0300

    Green-gradient

--- themes/index.js
+++ themes/index.js
@@ -336,6 +336,13 @@ const themes = {
     text_color: "61ffca",
     bg_color: "15141b",
   },
+  green_gradient: {
+    title_color: "00341B",
+    icon_color: "0CF574",
+    text_color: "2F97C1",
+    bg_color: "000000","00341B","00C064";
+
+},
 };
 
 module.exports = themes;
